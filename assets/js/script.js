94d82148d0a9f4c96ab3f4df6ac23c235af9b255
Robin Tapiru <robintapiru0894@gmail.com>
2022-11-29 20:08:01 +0400

    coka

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,26 @@ $( ".web .navigation-content.cs" ).hover(
     $('.banner-hover-3').css("display","none");
   }
 );
+
+
+$('.input-message').keyup(function(){
+  if($(this).val()){
+      $('button.send').addClass("active");
+  }else{
+      $('button.send').removeClass("active");
+  }
+});
+
+  function closeViewMedia() {
+    $('.view-media-container').fadeOut();
+    $('body').css('overflow-y','scroll')
+  }
+  $('div.portfolio-content').click(function(){
+    if(window.innerWidth > 1023) {
+      $('.view-media-container').fadeIn().css('display','flex');
+      $('body').css('overflow-y','hidden');
+    }
+  });
+  $('button.send').click(function(){
+    alert('messange sent!!!');
+  });
